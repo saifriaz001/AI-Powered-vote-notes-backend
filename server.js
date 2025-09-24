304ac9758f33b53c0908e6bd992b11df58c949b8
saifriaz001 <89684150+saifriaz001@users.noreply.github.com>
2025-09-24 11:04:13 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: [ "http://localhost:5173"], // allow all origins
+  origin: [ "http://localhost:5173","https://ai-powered-vote-notes-frontend.vercel.app" ], // allow all origins
   methods: ["GET", "POST", "PUT", "DELETE"], // allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // allowed headers
   credentials: true, 
